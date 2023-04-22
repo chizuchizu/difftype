d995deefec679a7b19ccbfa53f239e70caf0cde4
Ochi Yuma <ochimasumi@gmail.com>
2023-04-22 16:38:47 +0900

    add proto

---
+++ wip/proto.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+  "fmt"
+  "time"
+  // "bufio"
+  "io/ioutil"
+  "os"
+  "os/exec"
+  "github.com/pterm/pterm"
+)
+
+
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+
+func check_same_file(pathA string, pathB string) (bool) {
+  contA, errA := ioutil.ReadFile(pathA)
+  contB, errB := ioutil.ReadFile(pathB)
+
+  if errA != nil || errB != nil {
+    return false;
+  }
+
+  if string(contA) == string(contB) {
+    return true;
+  } else { 
+    return false;
+  }
+}
+
+
+func main(){
+  area, _ := pterm.DefaultArea.Start()
+  fmt.Println("start")
+
+  // Countdown
+  for i := 3; i >= 0; i-- {
+    base_str := fmt.Sprintf("Foo %d", i)
+
+    str, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString(base_str)).Srender()
+    str = pterm.DefaultCenter.Sprint(str)
+    area.Update(str)
+
+    time.Sleep(time.Second)
+  }
+
+  startTime := time.Now()
+
+  srcPath := "i.txt"
+  dstPath := "t.txt"
+  filePath := "target.txt"
+
+  cmd := exec.Command("cp", srcPath, filePath)
+  _, err := cmd.Output()
+  check(err)
+
+  f, err := os.Open(filePath)
+  check(err)
+  defer f.Close()
+
+  pterm.DefaultBasicText.Println("Hello, World!")
+
+  for {
+    isSame := check_same_file(filePath, dstPath)
+    if isSame {
+      break
+    } else {
+      time.Sleep(100 * time.Millisecond)
+    }
+  }
+
+  endTime := time.Now()
+  elapsedTime := endTime.Sub(startTime)
+
+  showStr := fmt.Sprintf("\n Time: %s", elapsedTime)
+
+  str, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("The End")).Srender()
+  str = pterm.DefaultCenter.Sprint(str)
+  area.Update(str)
+  
+  pterm.DefaultBasicText.Println(showStr)
+  time.Sleep(1 * time.Millisecond)
+
+
+  area.Stop()
+
+}
