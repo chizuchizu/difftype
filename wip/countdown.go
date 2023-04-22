ab88c0529c958579b9e81619867f52f47c183118
Ochi Yuma <ochimasumi@gmail.com>
2023-04-22 13:41:45 +0900

    tab -> space

--- wip/countdown.go
+++ wip/countdown.go
@@ -2,21 +2,22 @@ package main
 
 import (
   "fmt"
-	"time"
+  "time"
 
-	"github.com/pterm/pterm"
+  "github.com/pterm/pterm"
 )
 
 
 func main(){
   area, _ := pterm.DefaultArea.Start()
-	// area, _ := pterm.DefaultArea.WithCenter().Start()
 
   for i := 5; i >= 0; i-- {
     base_str := fmt.Sprintf("Foo %d", i)
+
     str, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString(base_str)).Srender()
     str = pterm.DefaultCenter.Sprint(str)
     area.Update(str)
+
     time.Sleep(time.Second)
   }
 
